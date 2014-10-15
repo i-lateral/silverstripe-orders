14ea517fa24a2552ce9c638bf7913b674cf3f443
Morven Lewis-Everley <morven@i-lateral.com>
2014-10-15 09:36:13 +0100

    Remove product controller extension from this module (as it is being merged into the commerce moduel)

--- _config.php
+++ _config.php
@@ -2,6 +2,3 @@
 
 if(class_exists("Users_Account_Controller"))
     Users_Account_Controller::add_extension("CheckoutUserAccountControllerExtension");
-
-if(class_exists("CatalogueProductController"))
-    CatalogueProductController::add_extension("CheckoutCatalogueProductControllerExtension");
