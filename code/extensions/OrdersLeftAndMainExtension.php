31e9d64cb2bad7b39eb2711f0b06b1122bb65722
Morven Lewis-Everley <morven@i-lateral.com>
2015-02-13 09:30:20 +0000

    Change css injection into admin from an extension to a config change

--- code/extensions/OrdersLeftAndMainExtension.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class OrdersLeftAndMainExtension extends LeftAndMainExtension {
-    public function init() {
-        parent::init();
-
-        Requirements::css('orders/css/admin.css');
-    }
-}
