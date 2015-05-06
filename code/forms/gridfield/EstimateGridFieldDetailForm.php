432df9a12facdcd06ee6e7ce2d23973a6db2b4a3
Morven Lewis-Everley <morven@i-lateral.com>
2015-05-06 11:19:55 +0100

    Remove saved message from order forms and add redirect so that new estimates are saved and then redirected

--- code/forms/gridfield/EstimateGridFieldDetailForm.php
+++ code/forms/gridfield/EstimateGridFieldDetailForm.php
@@ -20,6 +20,38 @@ class EstimateGridFieldDetailForm_ItemRequest extends GridFieldDetailForm_ItemRe
 		'view',
 		'ItemEditForm'
 	);
+    
+    public function edit($request) {
+		$controller = $this->getToplevelController();
+		$form = $this->ItemEditForm($this->gridField, $request);
+
+		$return = $this->customise(array(
+			'Backlink' => $controller->hasMethod('Backlink') ? $controller->Backlink() : $controller->Link(),
+			'ItemEditForm' => $form,
+		))->renderWith($this->template);
+        
+        // If this is a new record, we need to save it first
+        if($this->record->ID == 0) {
+            $this->record->write();
+            
+            $controller
+                ->getRequest()
+                ->addHeader('X-Pjax', 'Content');
+            
+            return $controller->redirect($this->Link());
+        }
+        
+
+		if($request->isAjax()) {
+			return $return;	
+		} else {
+			// If not requested by ajax, we need to render it within the controller context+template
+			return $controller->customise(array(
+				// TODO CMS coupling
+				'Content' => $return,
+			));	
+		}
+	}
 
 	public function ItemEditForm() {
 		$form = parent::ItemEditForm();
@@ -36,7 +68,90 @@ class EstimateGridFieldDetailForm_ItemRequest extends GridFieldDetailForm_ItemRe
             );
 		}
         
+        // Set our custom template
+        $form->setTemplate("OrdersItemEditForm");
+        
 		return $form;
+	}
+    
+    // Overwrite default save so we can handle adding items even if the
+    // object doesn't exist yet
+    public function doSave($data, $form) {
+		$new_record = $this->record->ID == 0;
+		$controller = $this->getToplevelController();
+		$list = $this->gridField->getList();
+		
+		if($list instanceof ManyManyList) {
+			// Data is escaped in ManyManyList->add()
+			$extraData = (isset($data['ManyMany'])) ? $data['ManyMany'] : null;
+		} else {
+			$extraData = null;
+		}
+
+		if(!$this->record->canEdit()) {
+			return $controller->httpError(403);
+		}
+		
+		if (isset($data['ClassName']) && $data['ClassName'] != $this->record->ClassName) {
+			$newClassName = $data['ClassName'];
+			// The records originally saved attribute was overwritten by $form->saveInto($record) before.
+			// This is necessary for newClassInstance() to work as expected, and trigger change detection
+			// on the ClassName attribute
+			$this->record->setClassName($this->record->ClassName);
+			// Replace $record with a new instance
+			$this->record = $this->record->newClassInstance($newClassName);
+		}
+
+		try {
+            $this->record->update($data);
+			$this->record->write();
+            
+			$list->add($this->record, $extraData);
+		} catch(ValidationException $e) {
+			$form->sessionMessage($e->getResult()->message(), 'bad', false);
+			$responseNegotiator = new PjaxResponseNegotiator(array(
+				'CurrentForm' => function() use(&$form) {
+					return $form->forTemplate();
+				},
+				'default' => function() use(&$controller) {
+					return $controller->redirectBack();
+				}
+			));
+			if($controller->getRequest()->isAjax()){
+				$controller->getRequest()->addHeader('X-Pjax', 'CurrentForm');
+			}
+			return $responseNegotiator->respond($controller->getRequest());
+		}
+
+		// TODO Save this item into the given relationship
+
+		$link = '<a href="' . $this->Link('edit') . '">"' 
+			. htmlspecialchars($this->record->Title, ENT_QUOTES) 
+			. '"</a>';
+		$message = _t(
+			'GridFieldDetailForm.Saved', 
+			'Saved {name} {link}',
+			array(
+				'name' => $this->record->i18n_singular_name(),
+				'link' => $link
+			)
+		);
+		
+		$form->sessionMessage($message, 'good', false);
+
+		if($new_record) {
+			return $controller->redirect($this->Link());
+		} elseif($this->gridField->getList()->byId($this->record->ID)) {
+			// Return new view, as we can't do a "virtual redirect" via the CMS Ajax
+			// to the same URL (it assumes that its content is already current, and doesn't reload)
+			return $this->edit($controller->getRequest());
+		} else {
+			// Changes to the record properties might've excluded the record from
+			// a filtered list, so return back to the main view if it can't be found
+			$noActionURL = $controller->removeAction($data['url']);
+			$controller->getRequest()->addHeader('X-Pjax', 'Content'); 
+			return $controller->redirect($noActionURL, 302); 
+		}
 	}
 
 
