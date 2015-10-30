5940f9934b57b9eff944d03c5c704c33ab029ac7
Morven Lewis-Everley <morven@i-lateral.com>
2015-10-30 14:08:37 +0000

    Add default address fields that pull dynamically from the default member address

--- code/extensions/CheckoutMemberExtension.php
+++ code/extensions/CheckoutMemberExtension.php
@@ -7,6 +7,14 @@
  * @package checkout
  */
 class CheckoutMemberExtension extends DataExtension {
+    
+    /**
+     * Cache an address object for if we need to us it again.
+     * 
+     * @var MemberAddress
+     */
+    private $cached_address;
+    
     private static $db = array(
         "PhoneNumber"   => "Varchar",
         "Company"       => "Varchar(99)"
@@ -14,6 +22,14 @@ class CheckoutMemberExtension extends DataExtension {
 
     private static $has_many = array(
         "Addresses"     => "MemberAddress"
+    );
+    
+    private static $casting = array(
+        'Address1'          => 'Varchar',
+        'Address2'          => 'Varchar',
+        'City'              => 'Varchar',
+        'PostCode'          => 'Varchar',
+        'Country'           => 'Varchar'
     );
 
     public function updateCMSFields(FieldList $fields) {
@@ -32,6 +48,74 @@ class CheckoutMemberExtension extends DataExtension {
         );
 
         return $fields;
+    }
+    
+    
+    /**
+     * Get the default address from our list of addreses. If no default
+     * is set, we should return the first in the list.
+     * 
+     * @return MemberAddress
+     */
+    public function getDefaultAddress() {
+        if($this->cached_address) {
+            return $this->cached_address;
+        } else {
+            $address = $this
+                ->owner
+                ->Addresses()
+                ->sort("Default", "DESC")
+                ->first();
+                
+            $this->cached_address = $address;
+            
+            return $address;
+        }
+    }
+    
+    /**
+     * Get address line one from our default address
+     * 
+     * @return String
+     */
+    public function getAddress1() {
+        if($address = $this->owner->getDefaultAddress())
+            return $address->Address1;
+    }
+    
+    /**
+     * Get address line two from our default address
+     * 
+     * @return String
+     */
+    public function getAddress2() {
+        if($address = $this->owner->getDefaultAddress())
+            return $address->Address2;
+    }
+    
+    /**
+     * Get city from our default address
+     * 
+     * @return String
+     */
+    public function getCity() {
+        if($address = $this->owner->getDefaultAddress())
+            return $address->City;
+    }
+    
+    public function getPostCode() {
+        if($address = $this->owner->getDefaultAddress())
+            return $address->PostCode;
+    }
+    
+    /**
+     * Get country from our default address
+     * 
+     * @return String
+     */
+    public function getCountry() {
+        if($address = $this->owner->getDefaultAddress())
+            return $address->Country;
     }
 
     /**
