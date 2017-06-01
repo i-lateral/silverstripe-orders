569cbf065c1c54d0f72116e64a686ce43ca32f09
Morven Lewis-Everley <morven@i-lateral.com>
2017-06-01 11:49:43 +0100

    Tidy up requires in _config.php

--- _config.php
+++ _config.php
@@ -3,17 +3,10 @@
 // If subsites is installed
 if(class_exists('Users_Account_Controller')) {
     Users_Account_Controller::add_extension('OrdersUserAccountControllerExtension');
-    Member::add_extension('OrdersMemberExtension');
-    Users_Account_Controller::add_extension("CheckoutUserAccountControllerExtension");
 }
 
 // If subsites is installed
 if(class_exists('Subsite')) {
     Order::add_extension('SubsitesOrdersExtension');
     OrderAdmin::add_extension('SubsiteMenuExtension');
-}
-
-// If subsites is installed
-if(class_exists('SiteConfig')) {
-    SiteConfig::add_extension('OrdersSiteConfigExtension');
 }
